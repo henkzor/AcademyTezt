8cbed7c31f263659a0286d13d94ca3379dbd4a4e
henkzor <93.henrik.karlsson@gmail.com>
2019-03-13 09:25:09 +0100

    Här har jag committat

--- AcademyTestWoppie/AcademyTestWoppie/Program.cs
+++ AcademyTestWoppie/AcademyTestWoppie/Program.cs
@@ -7,6 +7,8 @@ namespace AcademyTestWoppie
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
         }
     }
 }
