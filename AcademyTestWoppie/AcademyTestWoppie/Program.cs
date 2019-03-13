95638e94a3667043e9c76f863f164a5ecfa05cf7
henkzor <93.henrik.karlsson@gmail.com>
2019-03-13 09:24:02 +0100

    Initial commit

---
+++ AcademyTestWoppie/AcademyTestWoppie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcademyTestWoppie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
